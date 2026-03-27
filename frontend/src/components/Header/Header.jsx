94481016f0ef2c7e86f959e2b3013cd5aba3a221
ARYA RCB <superez.arya@gmail.com>
2026-03-27 19:53:59 +0530

    Fooddy the delivery web

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredient and culinary expertise.Our mission is to satisfy your craving and elevate your dining experience , one delicious meal at a time </p>
+        <button>View menu</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Header
